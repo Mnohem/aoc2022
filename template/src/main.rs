85bc8ae728a8743f519bf5c69693551a0e6ab2fa
Mnohem <stcefalu@gmail.com>
2022-11-27 20:08:17 -0800

    Added 'mod solve' to template for solve checking

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,6 +1,6 @@
 use helper::{resolve, when};
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let input = {
         let run_arg = std::env::args().next().unwrap();
         let day = run_arg.split('/').last().unwrap();
@@ -8,6 +8,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
 
     println!("{}", input);
+}
 
-    Ok(())
+pub fn day#_p1(input: &str) -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
+
+pub fn day#_p2(input: &str) -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
+
+#[cfg(test)]
+mod solve {
+    #[test]
+    fn solved_p1() {
+        assert!(false);
+    }
+
+    #[test]
+    fn solved_p2() {
+        assert!(false);
+    }
 }
