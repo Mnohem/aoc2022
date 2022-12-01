5e49baf4fdff6031742a770fd306ad9c5061554d
Mnohem <stcefalu@gmail.com>
2022-11-30 20:39:55 -0800

    Started day1

---
+++ day1/src/main.rs
@@ -0,0 +1,16 @@
+use helper::*;
+use once_cell::sync::Lazy;
+
+static INPUT: Lazy<String> = Lazy::new(|| helper::easy_aoc_input());
+
+fn main() {
+    println!("{}", *INPUT);
+}
+
+pub fn day1_p1() -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
+
+pub fn day1_p2() -> Result<(), Box<dyn std::error::Error>> {
+    todo!()
+}
